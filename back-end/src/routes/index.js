da819997ab7d51b3f56ac9bd8fe7de7e9bdf7191
Ali.Radmanesh <ali.radmanesh1995@gmail.com>
2022-07-26 00:56:55 +0430

    feat: Add logInRoute

--- back-end/src/routes/index.js
+++ back-end/src/routes/index.js
@@ -1,4 +1,5 @@
 import { testRoute } from "./testRoute";
 import { signUpRoute } from "./signUpRoute";
+import { logInRoute } from "./logInRoute";
 
-export const routes = [testRoute, signUpRoute];
+export const routes = [testRoute, signUpRoute, logInRoute];
