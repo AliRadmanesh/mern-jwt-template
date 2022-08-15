a9bf888947cf253b8b96bbd66321c251646c299a
Ali Radmanesh <ali.radmanesh1995@gmail.com>
2022-08-16 00:58:21 +0430

    Add verify-email route on the backend

--- back-end/src/routes/index.js
+++ back-end/src/routes/index.js
@@ -2,5 +2,12 @@ import { testRoute } from "./testRoute";
 import { signUpRoute } from "./signUpRoute";
 import { logInRoute } from "./logInRoute";
 import { updateUserInfoRoute } from "./updateUserInfoRoute";
+import { verifyEmailRoute } from "./verifyEmailRoute";
 
-export const routes = [testRoute, signUpRoute, logInRoute, updateUserInfoRoute];
+export const routes = [
+  testRoute,
+  signUpRoute,
+  logInRoute,
+  updateUserInfoRoute,
+  verifyEmailRoute,
+];
